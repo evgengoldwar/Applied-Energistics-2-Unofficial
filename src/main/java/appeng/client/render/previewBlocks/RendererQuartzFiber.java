69465c914ad21831a8db41881d0bc9c4bb6d1b00
evgengoldwar <evgeniy10102003kuptsov@gmail.com>
2025-11-22 20:21:42 +0400

    Cleanup code

--- src/main/java/appeng/client/render/previewBlocks/RendererQuartzFiber.java
+++ src/main/java/appeng/client/render/previewBlocks/RendererQuartzFiber.java
@@ -2,62 +2,13 @@ package appeng.client.render.previewBlocks;
 
 import java.util.List;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.common.util.ForgeDirection;
-
-import org.lwjgl.opengl.GL11;
-
 import appeng.parts.networking.PartQuartzFiber;
 
 public class RendererQuartzFiber extends AbstractRendererPreview implements IRenderPreview {
 
     @Override
     public void renderPreview() {
-        EntityPlayer player = ViewHelper.getPlayer();
-        if (player == null) return;
-
-        double playerX = player.lastTickPosX
-                + (player.posX - player.lastTickPosX) * ViewHelper.getCurrentPartialTicks();
-        double playerY = player.lastTickPosY
-                + (player.posY - player.lastTickPosY) * ViewHelper.getCurrentPartialTicks();
-        double playerZ = player.lastTickPosZ
-                + (player.posZ - player.lastTickPosZ) * ViewHelper.getCurrentPartialTicks();
-
-        GL11.glPushMatrix();
-        GL11.glTranslated(-playerX, -playerY, -playerZ);
-
-        GL11.glEnable(GL11.GL_BLEND);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
-        GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_CULL_FACE);
-        GL11.glDepthMask(false);
-
-        getValidColorGL11();
-
-        boolean shouldPlaceOnNeighborBlock = shouldPlaceOnNeighborBlock();
-        int previewX = ViewHelper.getPreviewX();
-        int previewY = ViewHelper.getPreviewY();
-        int previewZ = ViewHelper.getPreviewZ();
-        ForgeDirection placementSide = ViewHelper.getPlacementSide();
-
-        if (shouldPlaceOnNeighborBlock) {
-            int fiberX = previewX + placementSide.offsetX;
-            int fiberY = previewY + placementSide.offsetY;
-            int fiberZ = previewZ + placementSide.offsetZ;
-            applySideRotation(fiberX, fiberY, fiberZ, placementSide.getOpposite());
-        } else {
-            applySideRotation(previewX, previewY, previewZ, placementSide);
-        }
-
-        renderBase(6.0, 6.0, 10.0, 10.0, 10.0, 16.0);
-
-        GL11.glDepthMask(true);
-        GL11.glEnable(GL11.GL_CULL_FACE);
-        GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glPopMatrix();
+        renderCommonPreview(() -> { renderBase(6.0, 6.0, 10.0, 10.0, 10.0, 16.0); });
     }
 
     @Override
